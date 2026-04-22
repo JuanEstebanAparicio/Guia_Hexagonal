f7a27baedfe4cd4ae37ec7643bc664ae011889a6
JuanEstebanAparicio <apariciojuanesteban@gmail.com>
2026-04-22 13:28:18 -0500

    coreccion de rutas

--- src/Application/Ports/Out/GetUserByIdPort.php
+++ src/Application/Ports/Out/GetUserByIdPort.php
@@ -2,8 +2,8 @@
 
 declare(strict_types=1);
 
-require_once __DIR__ . '/../../Domain/Models/UserModel.php';
-require_once __DIR__ . '/../../Domain/ValueObjects/UserId.php';
+require_once __DIR__ . '/../../../Domain/Models/UserModel.php';
+require_once __DIR__ . '/../../../Domain/ValueObjects/UserId.php';
 
 interface GetUserByIdPort
 {
