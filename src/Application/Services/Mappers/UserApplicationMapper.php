0b2ac414116de5c4364f07da69e280e2735597f3
JuanEstebanAparicio <apariciojuanesteban@gmail.com>
2026-04-22 14:31:54 -0500

    UserMapper completado

--- src/Application/Services/Mappers/UserApplicationMapper.php
+++ src/Application/Services/Mappers/UserApplicationMapper.php
@@ -17,3 +17,70 @@ require_once __DIR__ . '/../../../Domain/ValueObjects/UserPassword.php';
 
 require_once __DIR__ . '/../../../Domain/Enums/UserStatusEnum.php';
 
+final class UserApplicationMapper
+{
+    public static function fromCreateCommandToModel(CreateUserCommand $command): UserModel
+    {
+        return new UserModel(
+            new UserId($command->getId()),
+            new UserName($command->getName()),
+            new UserEmail($command->getEmail()),
+            UserPassword::fromPlainText($command->getPassword()),
+            $command->getRole(),
+            UserStatusEnum::PENDING
+        );
+    }
+
+    public static function fromUpdateCommandToModel(UpdateUserCommand $command): UserModel
+    {
+        return new UserModel(
+            new UserId($command->getId()),
+            new UserName($command->getName()),
+            new UserEmail($command->getEmail()),
+            UserPassword::fromPlainText($command->getPassword()),
+            $command->getRole(),
+            $command->getStatus()
+        );
+    }
+
+        public static function fromGetUserByIdQueryToUserId(GetUserByIdQuery $query): UserId
+        {
+            return new UserId($query->getId());
+        }
+
+        public static function fromDeleteCommandToUserId(DeleteUserCommand $command): UserId
+        {
+            return new UserId($command->getId());
+        }
+        
+        /**
+        * @return array<string, string>
+        */
+        public static function fromModelToArray(UserModel $user): array
+        {
+            return array(
+                'id' => $user->id()->value(),
+                'name' => $user->name()->value(),
+                'email' => $user->email()->value(),
+                'password' => $user->password()->value(),
+                'role' => $user->role(),
+                'status' => $user->status()
+            );
+        }
+
+        /**
+        * @param UserModel[] $users
+        * @return array<int, array<string, string>>
+        */
+
+        public static function fromModelsToArray(array $users): array
+        {
+            $result = array();
+
+            foreach ($users as $user) {
+                $result[] = self::fromModelToArray($user);
+            }
+
+            return $result;
+        }
+}
